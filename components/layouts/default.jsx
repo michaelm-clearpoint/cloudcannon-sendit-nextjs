0cb2d2ca0053a2506cadc1b3f8f8611dec466251
Michael Magsuci <michael.magsuci@clearpoint.co.nz>
2024-03-19 13:27:39 +1300

    fix js path

--- components/layouts/default.jsx
+++ components/layouts/default.jsx
@@ -65,11 +65,11 @@ export default function DefaultLayout({ children, page }) {
             {children}
 			<Footer page={page}/>
 
-			<script src="/vendor/jQuery/jquery.min.js" async></script>
-			<script src="/vendor/bootstrap/bootstrap.bundle.min.js" async></script>
-			<script src="/vendor/counter-up/countup.js" async></script>
-			<script src="/vendor/magnific-popup/magnific-popup.min.js" async></script>
-			<script src="/js/script.js" defer></script>
+			<script src="./vendor/jQuery/jquery.min.js" async></script>
+			<script src="./vendor/bootstrap/bootstrap.bundle.min.js" async></script>
+			<script src="./vendor/counter-up/countup.js" async></script>
+			<script src="./vendor/magnific-popup/magnific-popup.min.js" async></script>
+			<script src="./js/script.js" defer></script>
 		</>
 	);
 }
